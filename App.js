5377d5f24dc18b803121d5f6680a1a3c585a55ee
chiruwebdeveloper <85474558+chiruwebdeveloper@users.noreply.github.com>
2021-06-07 13:49:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Note from './components/Note';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Note />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Note extends React.Component
+{
+    render(){
+        return( 
+            <div className="note">
+                <h1>Javascript and React.js</h1>
+                <p>This was an amazing Bootcamp taken by Shaurya Sir we covered eerything from scratch including javascript, React.js and HTML.</p>
+            </div>
+        );
+    }
+}
+export default Note;
